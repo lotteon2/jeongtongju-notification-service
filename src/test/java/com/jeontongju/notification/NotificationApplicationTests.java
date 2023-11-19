ed6a1ce289e7b4577c27ae0dd24754ae23f47ced
최성훈 <33412452+realsuperman@users.noreply.github.com>
2023-11-19 16:33:18 +0900

    *

---
+++ src/test/java/com/jeontongju/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeontongju.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
