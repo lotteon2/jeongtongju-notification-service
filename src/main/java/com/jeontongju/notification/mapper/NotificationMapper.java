d317cf6c6caa3b56dfb5312cafc0a941efc37d8a
woo yup Lee <zjadlspun1114@naver.com>
2023-12-25 16:44:05 +0900

    FEAT 알림 내역 및 안읽은 알림 개수 로직 작성 SCRUM-136

--- src/main/java/com/jeontongju/notification/mapper/NotificationMapper.java
+++ src/main/java/com/jeontongju/notification/mapper/NotificationMapper.java
@@ -2,9 +2,14 @@ package com.jeontongju.notification.mapper;
 
 import com.jeontongju.notification.domain.Notification;
 
+import com.jeontongju.notification.dto.response.NotificationInfoForInquiryResponseDto;
+import com.jeontongju.notification.dto.response.NotificationInfoForSingleInquiryDto;
 import io.github.bitbox.bitbox.enums.NotificationTypeEnum;
 import io.github.bitbox.bitbox.enums.RecipientTypeEnum;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class NotificationMapper {
@@ -18,6 +23,33 @@ public class NotificationMapper {
         .recipientId(recipientId)
         .recipientTypeEnum(recipientTypeEnum)
         .notificationTypeEnum(notificationTypeEnum)
+        .build();
+  }
+
+  public List<NotificationInfoForSingleInquiryDto> toListLookupDto(
+      List<Notification> notifications) {
+
+    List<NotificationInfoForSingleInquiryDto> inquiryResponseDtos = new ArrayList<>();
+    for (Notification notification : notifications) {
+
+      NotificationInfoForSingleInquiryDto build =
+          NotificationInfoForSingleInquiryDto.builder()
+              .notificationId(notification.getNotificationId())
+              .notificationType(notification.getNotificationTypeEnum())
+              .isRead(notification.getIsRead())
+              .createdAt(notification.getCreatedAt())
+              .build();
+      inquiryResponseDtos.add(build);
+    }
+    return inquiryResponseDtos;
+  }
+
+  public NotificationInfoForInquiryResponseDto toInquiryDto(
+      int notReadCounts, List<NotificationInfoForSingleInquiryDto> notifications) {
+
+    return NotificationInfoForInquiryResponseDto.builder()
+        .notReadcounts(notReadCounts)
+        .notifications(notifications)
         .build();
   }
 }
