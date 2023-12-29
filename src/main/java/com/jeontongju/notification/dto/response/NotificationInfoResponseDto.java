3e36de299cec3f59980b5ed555068a9b3d588d94
woo yup Lee <zjadlspun1114@naver.com>
2023-12-29 10:23:30 +0900

    FIX 알림 조회 시 필요한 notificationId 전달

---
+++ src/main/java/com/jeontongju/notification/dto/response/NotificationInfoResponseDto.java
@@ -0,0 +1,16 @@
+package com.jeontongju.notification.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class NotificationInfoResponseDto {
+
+  private Long notificationId;
+  private Object data;
+}
